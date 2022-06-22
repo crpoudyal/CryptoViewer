2c4620c0e3698034c935756ffbac21557f02a5a7
crpoudyal <crpoudyal@gmail.com>
2022-06-22 13:18:20 +0545

    theme data with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cryptoviewer/constants/themes.dart';
 import 'package:cryptoviewer/providers/marketdata.dart';
+import 'package:cryptoviewer/providers/theme_provider.dart';
 import 'package:cryptoviewer/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,12 +18,22 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider<MarketData>(
           create: (context) => MarketData(),
-        )
+        ),
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (context) => ThemeProvider(),
+        ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Crypto Viewer',
-        home: HomeScreen(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: themeProvider.themeMode,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            title: 'Crypto Viewer',
+            home: HomeScreen(),
+          );
+        },
       ),
     );
   }
